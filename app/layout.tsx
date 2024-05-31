2a2b40c17b48d91883fde776b8d25bb4c8ae3bb5
luisdelotavo <luisdelotavo@outlook.com>
2024-05-30 22:14:23 -0400

    final touches

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Luis Delotavo's Portfolio",
-  description: "To showcase ...",
+  description: "Explore my projects, technical skills, and professional experience.",
 };
 
 interface RootLayoutProps {
@@ -17,6 +17,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
       );
